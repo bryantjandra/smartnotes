2f2d3c2b145a93be15f7c5760aa6efe6d73abad4
Bryan Tjandra <tjandra.benedict@gmail.com>
2024-07-22 19:16:06 +0800

    reformat header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 function Header() {
   return (
-    <header className="flex items-center justify-between gap-4 p-4">
+    <header className="flex items-center justify-between gap-4 p-4 absolute top-0 left-10 right-10">
       <h1 className="font-medium">
-        transla<span className="text-blue-400 bold">scribe</span>
+        transla<span className="text-blue-400 font-bold">scribe</span>
       </h1>
       <button className="flex items-center gap-2 specialBtn px-4 py-2 rounded-lg text-blue-400">
         <p>New</p>
