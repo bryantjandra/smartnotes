bfca63aab25e48b962e8fcd995446c9fc078bf93
Bryan Tjandra <tjandra.benedict@gmail.com>
2024-07-28 23:47:33 +0800

    add audio player with dynamic source handling

--- src/components/FileDisplay.jsx
+++ src/components/FileDisplay.jsx
@@ -1,14 +1,34 @@
+import React, { useRef, useEffect } from "react";
+
 function FileDisplay(props) {
   const { handleAudioReset, file, audioStream, handleFormSubmission } = props;
 
+  const audioRef = useRef();
+
+  useEffect(() => {
+    if (!file && !audioStream) {
+      return;
+    }
+    if (file) {
+      audioRef.current.src = URL.createObjectURL(file);
+    } else {
+      audioRef.current.src = URL.createObjectURL(audioStream);
+    }
+  }, [audioStream, file]);
+
   return (
-    <main className="flex-1 p-4 flex flex-col justify-center gap-3 sm:gap-4 pb-20 w-72 max-w-full sm:w-96 mx-auto ">
+    <main className="flex-1 p-4 flex flex-col justify-center gap-3 sm:gap-4 pb-20 w-full max-w-prose mx-auto ">
       <h1 className="font-semibold text-center text-5xl sm:text-6xl md:text-7xl text-white">
         Your&nbsp;<span className="text-indigo-400 font-bold">File</span>
       </h1>
       <div className="flex flex-col text-left mb-4 my-4">
         <h3 className="font-semibold text-white">Name:</h3>
         <p className="text-slate-400 ">{file ? file?.name : "Custom Audio"}</p>
+      </div>
+      <div className="flex flex-col mb-2">
+        <audio ref={audioRef} className="w-full" controls>
+          Your browser does not support the audio element
+        </audio>
       </div>
       <div className="flex items-center justify-between gap-4">
         <button
